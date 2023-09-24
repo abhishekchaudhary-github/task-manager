7156062187d018a1d77453a3f4f46e068918d847
abhishekchaudhary-github <abhishekch.165@gmail.com>
2023-09-24 12:45:13 +0530

    page not found

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const tasks = require('./tasks/tasks')
 const express = require('express')
 const app = express();
 const url = process.env.mongo_uri
+const notFound = require('./middleware/notFound')
 
 app.use(express.json());
 app.use('/api/tasks/v1',tasks)
+app.use(notFound)
 const fn=async()=>{
     await mongoconnec(url)
     app.listen(8080,()=>{console.log('server up')})
