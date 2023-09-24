bb92a578a459f1cfbc3b57778544966cea274f64
abhishekchaudhary-github <abhishekch.165@gmail.com>
2023-09-24 23:54:57 +0530

    error handling part 2

---
+++ error/customError.js
@@ -0,0 +1,12 @@
+class customError extends Error {
+    constructor(mess,status) {
+        super(mess);
+        this.status = status;
+    }
+}
+
+const customError1   = (message,status) => {
+    return new customError(message,status)
+}
+
+module.exports = {customError1}
